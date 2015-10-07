64fe9bbb05d798327a7b64bd2e49ee482906cb0e
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2015-10-08 01:00:54 +0200

    Added unti test for servlet

---
+++ aem-script-console-bundle/src/test/java/net/thartm/aem/asconsole/groovy/servlets/GroovyShellRunnerServletTest.java
@@ -0,0 +1,23 @@
+package net.thartm.aem.asconsole.groovy.servlets;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author thomas.hartmann@netcentric.biz
+ * @since 10/2015
+ */
+public class GroovyShellRunnerServletTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testHandleRequest() throws Exception {
+        // map request to script object and call executer service
+    }
+}
