e96b877b7d01a5459a5c418152d9c98c0c33be24
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2015-10-15 12:29:16 +0200

    Rmoved obsolete modeliste setup

--- aem-script-console-content/src/main/content/jcr_root/apps/aemscriptconsole/clientlibs/js/aemscriptconsole.js
+++ aem-script-console-content/src/main/content/jcr_root/apps/aemscriptconsole/clientlibs/js/aemscriptconsole.js
@@ -7,15 +7,6 @@ var AemScriptConsole = function () {
             var editor = ace.edit("editor");
             editor.setTheme("ace/theme/twilight");
             (function () {
-                var modelist = ace.require("ace/ext/modelist");
-                // the file path could come from an xmlhttp request, a drop event,
-                // or any other scriptable file loading process.
-                // Extensions could consume the modelist and use it to dynamically
-                // set the editor mode. Webmasters could use it in their scripts
-                // for site specific purposes as well.
-                //var filePath = "blahblah/weee/some.js";
-                //var mode = modelist.getModeForPath(filePath).mode;
-                //console.log(mode);
                 editor.session.setMode("ace/mode/groovy");
             }());
         },
