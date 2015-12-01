26dd2f22eb4084456042de43c60a55f9c4e7d8a0
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2015-12-01 16:27:37 +0100

    added form post support

--- aem-script-console-bundle/src/main/java/net/thartm/aem/asconsole/script/servlets/ScriptPersistenceServlet.java
+++ aem-script-console-bundle/src/main/java/net/thartm/aem/asconsole/script/servlets/ScriptPersistenceServlet.java
@@ -22,7 +22,7 @@ import java.util.Map;
  * @author thomas.hartmann@netcentric.biz
  * @since 10/2015
  */
-@SlingServlet(selectors = "savescript", methods = { "POST" }, extensions = { "json" }, metatype = true)
+@SlingServlet(paths = {"/bin/private/acfs/script/save"}, methods = { "POST" }, extensions = { "json" }, metatype = true)
 public class ScriptPersistenceServlet extends AbstractJsonPostHandlerServlet {
 
     private static final String SCRIPT = "script";
