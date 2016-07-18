c67b298a90aa124e3d137be4d798fbe5e14bf48d
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2016-07-18 14:44:56 +0200

    Added card view. Introduced sling models. Added script model bean

---
+++ aem-script-console-bundle/src/main/java/net/thartm/aem/asconsole/models/Path.java
@@ -0,0 +1,10 @@
+package net.thartm.aem.asconsole.models;
+
+/**
+ * Common paths and locations inside the repo
+ * 
+ * @author thomas.hartmann@netcentric.biz
+ * @since 07/2016
+ */
+public enum Path {
+}
