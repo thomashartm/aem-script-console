f9143e39266533e48a237f2ec47715a9652b2690
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2016-12-04 19:59:30 +0100

    Create servlet is now capable of creating real script containers including blank hello world script binaries.

--- scriptconsole-bundle/src/main/java/biz/netcentric/aem/scriptconsole/action/servlets/CreateScriptServlet.java
+++ scriptconsole-bundle/src/main/java/biz/netcentric/aem/scriptconsole/action/servlets/CreateScriptServlet.java
@@ -21,7 +21,6 @@ import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 
@@ -34,6 +33,7 @@ import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.commons.jcr.JcrUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Maps;
+import org.apache.sling.commons.mime.MimeTypeService;
 
 /**
  * @author thomas.hartmann@netcentric.biz
@@ -63,7 +63,7 @@ public class CreateScriptServlet extends AbstractJsonHandlerServlet implements W
             final String scriptName = request.getParameter("name");
             final String scriptType = request.getParameter("type");
 
-            final Resource scriptResource = createResource(request.getResourceResolver(), scriptName, scriptType);
+            final Resource scriptResource = createScriptContainer(request.getResourceResolver(), locationResource, scriptName, scriptType);
 
             return;
         }
@@ -72,22 +72,42 @@ public class CreateScriptServlet extends AbstractJsonHandlerServlet implements W
                 "Unable to create the script at the submitted location. Please check your arguments.");
     }
 
-    private Resource createResource(final ResourceResolver resourceResolver, final String name, final String type)
-            throws PersistenceException {
-        final Resource newScript = resourceResolver.getResource("/myresource");
-        final Map<String, Object> properties = Maps.newHashMap();
+    private Resource createScriptContainer(final ResourceResolver resourceResolver, final Resource location, final String name, final String type)
+            throws IOException, RepositoryException {
 
+
+        final Map<String, Object> properties = Maps.newHashMap();
         properties.put(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED);
         properties.put(SlingConstants.PROPERTY_RESOURCE_TYPE, "/apps/aemscriptconsole/components/script");
         properties.put("scriptType", type);
 
-        final Resource scriptResource = resourceResolver.create(newScript, JcrUtil.createValidName(name), properties);
+        final Resource scriptContainer = resourceResolver.create(location, JcrUtil.createValidName(name), properties);
+        createScriptResource(resourceResolver, scriptContainer, type);
+
         resourceResolver.commit();
 
-        return scriptResource;
+        return scriptContainer;
     }
 
-    private Node createScriptNode(final ResourceResolver resourceResolver, final String path, final String type)
+    private void createScriptResource(final ResourceResolver resourceResolver, final Resource location, final String extension)
+            throws IOException, RepositoryException {
+
+        final Optional<ScriptLanguage> extensionOptional = Stream.of(ScriptLanguage.values())
+                .filter(l -> l.getExtension().equals(extension))
+                .findFirst();
+
+        final ScriptLanguage language  = extensionOptional.map(l -> l).orElse(ScriptLanguage.GROOVY);
+
+        final Map<String, Object> properties = Maps.newHashMap();
+        properties.put(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_FILE);
+
+        final String name = String.format("script.%s", language.getExtension());
+        final Resource script = resourceResolver.create(location, name, properties);
+
+        createBinaryProperty(resourceResolver, script, language);
+    }
+
+    private Node createBinaryProperty(final ResourceResolver resourceResolver, final Resource location, final ScriptLanguage language)
             throws RepositoryException, IOException {
 
         final Session session = resourceResolver.adaptTo(Session.class);
@@ -95,17 +115,13 @@ public class CreateScriptServlet extends AbstractJsonHandlerServlet implements W
             throw new IllegalStateException("Unable to adapt resolver to session.");
         }
 
-        final Optional<ScriptLanguage> extensionOptional = Stream.of(ScriptLanguage.values())
-                .filter(l -> l.getExtension().equals(type))
-                .findFirst();
+        final Binary binary = createBindaryProperty(session, "println 'Hello World'");
 
-        final String extension = extensionOptional.map(l -> l.getExtension()).orElse(ScriptLanguage.GROOVY.getExtension());
-
-        Binary binary = createBindaryProperty(session, "println 'Hello World'");
-
-        final Node node = session.getNode(path);
-        node.setProperty("xyz", binary);
-        session.save();
+        final Node node = session.getNode(location.getPath());
+        final Node jcrContentNode = node.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_RESOURCE);
+        jcrContentNode.setProperty(JcrConstants.JCR_MIMETYPE, "application/octet-stream");
+        jcrContentNode.setProperty(JcrConstants.JCR_ENCODING, "utf-8");
+        jcrContentNode.setProperty(JcrConstants.JCR_DATA, binary);
 
         return node;
     }
