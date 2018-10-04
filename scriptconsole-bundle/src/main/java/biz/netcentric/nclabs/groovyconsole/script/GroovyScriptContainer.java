dd2b7845c3cce7a9a9db05b36b59bdc8af7c128a
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2018-10-04 09:49:56 +0200

    Optimized imports

---
+++ scriptconsole-bundle/src/main/java/biz/netcentric/nclabs/groovyconsole/script/GroovyScriptContainer.java
@@ -0,0 +1,10 @@
+package biz.netcentric.nclabs.groovyconsole.script;
+
+/**
+ * TODO - add javadoc
+ *
+ * @author thomas.hartmann@netcentric.biz
+ * @since 10/2018
+ */
+public interface GroovyScriptContainer {
+}
