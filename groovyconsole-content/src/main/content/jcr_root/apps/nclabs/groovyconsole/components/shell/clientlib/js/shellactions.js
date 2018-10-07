a672293e036a9890ce81dc06778f16309941e62d
Thomas Hartmann <thomas.hartmann@netcentric.biz>
2018-10-07 12:21:20 +0200

    Added missing JS clientib for shell execution. Set correct endpoint to new script runner servlet

---
+++ groovyconsole-content/src/main/content/jcr_root/apps/nclabs/groovyconsole/components/shell/clientlib/js/shellactions.js
@@ -0,0 +1,130 @@
+(function ($, document, graniteAuthor) {
+
+    const RUN_GROOVYSCRIPT_ENDPOINT = '/bin/nclabs/groovyconsole/execute';
+
+    $(document).on("click", ".execute-script-button", function (element) {
+        executeScript(element);
+    });
+
+    function encodeQueryData(data) {
+        return Object.keys(data).map(function (key) {
+            return [key, data[key]].map(encodeURIComponent).join("=");
+        }).join("&");
+    }
+
+    function executeScript(element) {
+        let scriptSource = document.getElementById("groovy-script-source");
+
+        let scriptLocation = scriptSource.dataset.scriptLocation;
+        if (!scriptLocation) {
+            // no script location to validate
+            return;
+        }
+
+        let queryParams = getQueryParameters(scriptLocation);
+        let url = RUN_GROOVYSCRIPT_ENDPOINT + "?" + encodeQueryData(queryParams);
+        let errorMessage;
+
+        $.ajax({
+            url,
+            type: "GET",
+            async: false,
+            success: function (data) {
+                try {
+                    console.log(data);
+                    writeScriptExecutionResults(data);
+                } catch (err) {
+                    errorMessage = Granite.I18n.get("Unexpected error {0}" + err)
+                }
+            },
+            error: function () {
+                errorMessage = Granite.I18n.get("Unexpected error while communicating with server");
+            }
+        });
+
+        if (errorMessage) {
+            return errorMessage;
+        }
+    }
+
+    function getQueryParameters(scriptLocation) {
+        let queryParams = {
+            "location": scriptLocation
+        };
+
+        // get all text field values
+        let fields = document.querySelectorAll("input.groovyconsole-field");
+        for (var i = 0; i < fields.length; i++) {
+            let textField = fields[i];
+            if (textField) {
+                queryParams[textField.name] = textField.value;
+            }
+        }
+
+        // get radio button values
+        let radios = document.querySelectorAll("coral-radio.groovyconsole-radio");
+        for (var i = 0; i < radios.length; i++) {
+            let radio = radios[i];
+            if (radio.checked) {
+                queryParams[radio.name] = radio.value;
+            }
+        }
+
+        return queryParams;
+    }
+
+    function writeMessageBox(data) {
+        var messageType = data.failed ? "error" : "success";
+
+        var timing = data.executionTime + " ms";
+        let message = "";
+        if (data.error) {
+            message += "<br />";
+            message += data.error;
+        }
+
+        var alertBox = new Coral.Alert();
+        alertBox.set({
+            variant: messageType,
+            header: {
+                innerHTML: timing
+            },
+            content: {
+                innerHTML: message
+            }
+        });
+        return alertBox;
+    }
+
+    function createContentElement(headerText, contentText) {
+        var contentElement = document.createElement("div");
+        var header = document.createElement("h3");
+        header.append(headerText);
+        contentElement.append(header);
+        contentElement.append(contentText);
+
+        return contentElement;
+    }
+
+    function writeScriptExecutionResults(data) {
+
+        var resultbox = document.getElementById("groovy-results-panel-el");
+
+        // clear all existing elements first
+        while (resultbox.firstChild) {
+            resultbox.removeChild(resultbox.firstChild);
+        }
+
+        var alertBox = writeMessageBox(data);
+        resultbox.append(alertBox);
+
+        if (data.output) {
+            resultbox.appendChild(createContentElement("Execution Output", data.output));
+        }
+
+        if (data.result) {
+            resultbox.appendChild(createContentElement("Execution Result", data.result));
+        }
+    }
+
+})(jQuery, document, Granite.author);
